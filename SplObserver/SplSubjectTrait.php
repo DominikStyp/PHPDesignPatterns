bc7638b2f17cb5a3ece1cee83128db8b384c90e8
Dominik <malmsteenforce@tlen.pl>
2016-08-17 20:20:59 +0200

    Update SplSubjectTrait.php

--- SplObserver/SplSubjectTrait.php
+++ SplObserver/SplSubjectTrait.php
@@ -1,9 +1,7 @@
 <?php
 
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This trait can be used in every class that has to implement SplSubject interface
  */
 
 trait SplSubjectTrait {
